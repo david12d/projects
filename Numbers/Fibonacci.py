77a1120fc0cd18c7e5cd092d4b638890f3638511
David Ding <david12ding@outlook.com>
2019-02-12 14:31:34 +1100

    Update Fibonacci.py

--- Numbers/Fibonacci.py
+++ Numbers/Fibonacci.py
@@ -4,3 +4,24 @@ Enter a number and have the program generate the Fibonacci sequence to that numb
 
 import sys
 
+print("This program is a Fibonacci Sequence Generator.\n")
+
+ while True:
+    try:
+        x = (int) input("Enter a number")
+    except ValueError:
+        print("Sorry, enter a whole number please")
+        continue
+    else: 
+        break
+
+i = 0  
+n = 1
+m = 1
+for a to x:
+    print(n)
+    n = m
+    m += n
+    i += 1
+    if i == 50:
+        print("")
